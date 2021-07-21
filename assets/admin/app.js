afdf8d3e7bf5275ed67ccd70b4447a4d23b45ad0
Joudrier Kevin <kev.joudrier@gmail.com>
2021-07-21 16:33:48 +0200

    Clean code.

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -15,11 +15,3 @@ ReactDOM.render(
     document.querySelector('.react-dashboard')
 )
 
-// import ModalCustomElement from "../CustomElements/Modal/ModalCustomElement";
-// import FormAddSkillCustom from "./Form/Skills/formAddSkillCustom";
-// import EditableItemCustomElements from "../CustomElements/EditableItem/EditableItemCustomElements";
-//
-// customElements.define('editable-item', EditableItemCustomElements)
-// customElements.define('modal-dialog', ModalCustomElement)
-// customElements.define('form-add-skill', FormAddSkillCustom)
-
