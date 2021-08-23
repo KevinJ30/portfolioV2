e1fb78513436da3b4fe9be0b790d009da1008013
Joudrier Kevin <kev.joudrier@gmail.com>
2021-08-23 18:22:31 +0200

    Création du CRUD projet.

---
+++ src/Controller/Admin/ProjectController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Configuration;
+use App\Entity\Projects;
+use App\Form\ConfigurationType;
+use App\Form\ProjectType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Gestion des projets
+ *
+ * @route("/Dashboard/project", name="ADMIN_PROJECT_")
+ **/
+class ProjectController extends CRUDController {
+    /**
+     * @var string $entity Nom de la class de l'entité
+     **/
+    protected string $entity = Projects::class;
+
+    /**
+     * @var string $templatePath Chemin vers les fichiers template
+     **/
+    protected string $templatePath = "admin/project";
+
+    /**
+     * @var array $actions Les actions des projets
+     **/
+    protected array $actions = [
+        'home' => 'ADMIN_PROJECT_HOME',
+        'create' => 'ADMIN_PROJECT_CREATE',
+        'edit' => 'ADMIN_PROJECT_EDIT',
+        'delete' => 'ADMIN_PROJECT_DELETE'
+    ];
+
+    /**
+     * @var string $name Nom du CRUD
+     **/
+    protected string $name = "project";
+
+    /**
+     * Affiche la liste des projets
+     *
+     * @route("/", name="HOME")
+     * @return Response
+     **/
+    public function index() : Response {
+        return $this->CRUDIndex(ProjectType::class, [
+            'title' => 'Titre du projet',
+            'url' => 'Lien vers le projet',
+            'thumb_url' => 'Image miniature',
+            'excerpt' => 'Description courte'
+        ]);
+    }
+
+    /**
+     * Créer un nouveau projet
+     *
+     * @route("/create", methods={"POST"}, name="CREATE")
+     * @return Response
+     */
+    public function create() : Response {
+        return $this->CRUDCreate(ProjectType::class);
+    }
+
+    /**
+     * Éditer un projet
+     *
+     * @route("/edit/{id}", name="EDIT")
+     *
+     * @param Projects $projet
+     * @return Response
+     */
+    public function edit(Projects $projet) : Response {
+        return $this->CRUDEdit(ProjectType::class, $projet);
+    }
+
+    /**
+     * Supprimer un projet
+     *
+     * @Route("/delete/{id}", name="DELETE", methods={"DELETE"})
+     *
+     * @param Projects $project
+     * @return JsonResponse
+     */
+    public function delete(Projects $project) : JsonResponse {
+        return $this->CRUDDelete($project);
+    }
+}
