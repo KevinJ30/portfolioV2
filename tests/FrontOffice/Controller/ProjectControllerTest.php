addbc1ba0799859a051e8f90b18153637e467448
KevinJ30 <contact@joudrier-kevin.fr>
2021-10-08 22:19:15 +0200

    Mise en place d'un message si aucun projet n'est présent.

--- tests/FrontOffice/Controller/ProjectControllerTest.php
+++ tests/FrontOffice/Controller/ProjectControllerTest.php
@@ -8,19 +8,23 @@ use App\DataFixtures\SkillFixtures;
 use App\DataFixtures\UserFixtures;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProjectControllerTest extends WebTestCase {
 
     protected AbstractDatabaseTool  $databaseTool;
-    protected ?KernelBrowser $client = null;
+    protected KernelBrowser $client;
 
     public function setup() : void {
         $this->client = static::createClient();
 
-        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
-        $fixtures = $this->databaseTool->withPurgeMode(true)->loadFixtures([
+        /** @var ContainerInterface $container */
+        $container = static::getContainer();
+
+        $this->databaseTool = $container->get(DatabaseToolCollection::class)->get();
+        $this->databaseTool->withPurgeMode(1)->loadFixtures([
             ConfigurationFixtures::class,
             UserFixtures::class,
             SkillFixtures::class,
@@ -29,6 +33,7 @@ class ProjectControllerTest extends WebTestCase {
     }
 
     /**
+     * Devrait renvoyer une réponse 200
      * @return void
      */
     public function testSuccessfullyResponseIndex() : void {
@@ -36,11 +41,33 @@ class ProjectControllerTest extends WebTestCase {
         $this->assertResponseIsSuccessful();
     }
 
+    /**
+     * Devrait afficher tous les projets de la base de données
+     **/
     public function  testDisplayAllProjects() : void {
         $crawler = $this->client->request('GET', '/projects');
         $projectCard = $crawler->filter('.projects__container .card');
         $this->assertCount(10, $projectCard);
     }
 
+    /**
+     * Devrait afficher un message si aucun projet n'est présent
+     * dans la base de données
+     **/
+    public function testNotContainProject() : void {
+        $this->databaseTool->withPurgeMode(1)->loadFixtures([
+            ConfigurationFixtures::class,
+            UserFixtures::class,
+            SkillFixtures::class
+        ]);
 
+        $crawler = $this->client->request('GET', '/projects');
+        $noContainMessage = $crawler->filter('p');
+        $this->assertSelectorTextContains('p', "Il n'y a encore aucun projet !");
+    }
+
+    public function testDisplaySixProjectForPaginate() : void {
+        $crawler = $this->client->request('GET', '/projects');
+        $this->assertCount(6, $crawler->filter('.projects__container .card'));
+    }
 }
